637b8c52738ad22b28e366d659ed4a3422ee12af
Matheo C <62748695+capritkt@users.noreply.github.com>
2025-11-20 10:33:50 +0100

    Init - ProxiBanque init
    proxi banque project init

---
+++ ProxiBanque/src/main/java/com/matheo/chahwan/proxibanque/ProxiBanqueApplication.java
@@ -0,0 +1,13 @@
+package com.matheo.chahwan.proxibanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxiBanqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxiBanqueApplication.class, args);
+    }
+
+}
